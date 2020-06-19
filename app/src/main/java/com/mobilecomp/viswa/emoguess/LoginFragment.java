341d0446c11ca8b7be18047f41d792dac9069ca0
Shubhangi <shubhangi17364@iiitd.ac.in>
2020-06-20 03:06:39 +0530

    .

--- app/src/main/java/com/mobilecomp/viswa/emoguess/LoginFragment.java
+++ app/src/main/java/com/mobilecomp/viswa/emoguess/LoginFragment.java
@@ -1,8 +1,14 @@
 package com.mobilecomp.viswa.emoguess;
 
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.text.TextUtils;
@@ -90,7 +96,8 @@ public class LoginFragment extends Fragment {
                 fAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
-                        if(task.isSuccessful()){
+                        isConnected(getContext());
+                        if(task.isSuccessful() && isConnected(getContext())==true){
                             Toast.makeText(getContext(),"Logged in Successfully ",Toast.LENGTH_SHORT).show();
                             startActivity(new Intent(getContext(),HomeActivity.class));
                         }
@@ -109,12 +116,48 @@ public class LoginFragment extends Fragment {
         txtRegister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(getContext(),RegisterActivity.class));
+                isConnected(getContext());
+                if (isConnected(getContext())==true) {
+                    startActivity(new Intent(getContext(),RegisterActivity.class));
+                }
             }
         });
 
         return view;
     }
+
+    public boolean isConnected(Context context) {
+        ConnectivityManager connectivityManager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo wifiInfo = connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
+        NetworkInfo mobileInfo = connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_MOBILE);
+
+        if ((wifiInfo != null && wifiInfo.isConnected()) || (mobileInfo != null && mobileInfo.isConnected())) {
+            return true;
+        } else {
+            showDialog();
+            return false;
+        }
+    }
+
+    private void showDialog()
+    {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setMessage("Connect to wifi or quit")
+                .setCancelable(false)
+                .setPositiveButton("Connect to WIFI", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        startActivity(new Intent(Settings.ACTION_WIFI_SETTINGS));
+                    }
+                })
+                .setNegativeButton("Quit", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        getActivity().finish();
+                    }
+                });
+        AlertDialog alert = builder.create();
+        alert.show();
+    }
+
     public interface OnFragmentInteractionListener {
         // TODO: Update argument type and name
         void onFragmentInteraction(Uri uri);
