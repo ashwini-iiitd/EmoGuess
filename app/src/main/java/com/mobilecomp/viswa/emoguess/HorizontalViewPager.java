a30e86c4e22f17074e2ee0706d2cc212dcabc95e
Shubhangi <shubhangi17364@iiitd.ac.in>
2020-11-07 18:41:45 +0530

    .

--- app/src/main/java/com/mobilecomp/viswa/emoguess/HorizontalViewPager.java
+++ app/src/main/java/com/mobilecomp/viswa/emoguess/HorizontalViewPager.java
@@ -20,48 +20,48 @@ public class HorizontalViewPager extends ViewPager {
 //        if (!ImageFragment.timerrunning){
 //            setMyScroller();
 //        }
-        //setMyScroller();
+        setMyScroller();
     }
 
     public HorizontalViewPager(Context context, AttributeSet attrs) {
         super(context, attrs);
         init();
-       // setMyScroller();
+        setMyScroller();
     }
 
-//    @Override
-//    public boolean onInterceptTouchEvent(MotionEvent event) {
-//        // Never allow swiping to switch between pages
-//        return false;
-//    }
-//
-//    @Override
-//    public boolean onTouchEvent(MotionEvent event) {
-//        // Never allow swiping to switch between pages
-//        return false;
-//    }
-//
-//    public void setMyScroller() {
-//        try {
-//            Class<?> viewpager = ViewPager.class;
-//            Field scroller = viewpager.getDeclaredField("mScroller");
-//            scroller.setAccessible(true);
-//            scroller.set(this, new MyScroller(getContext()));
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
-//    }
-//
-//    public class MyScroller extends Scroller {
-//        public MyScroller(Context context) {
-//            super(context, new DecelerateInterpolator());
-//        }
-//
-//        @Override
-//        public void startScroll(int startX, int startY, int dx, int dy, int duration) {
-//            super.startScroll(startX, startY, dx, dy, 350 /*1 secs*/);
-//        }
-//    }
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent event) {
+        // Never allow swiping to switch between pages
+        return false;
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        // Never allow swiping to switch between pages
+        return false;
+    }
+
+    public void setMyScroller() {
+        try {
+            Class<?> viewpager = ViewPager.class;
+            Field scroller = viewpager.getDeclaredField("mScroller");
+            scroller.setAccessible(true);
+            scroller.set(this, new MyScroller(getContext()));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public class MyScroller extends Scroller {
+        public MyScroller(Context context) {
+            super(context, new DecelerateInterpolator());
+        }
+
+        @Override
+        public void startScroll(int startX, int startY, int dx, int dy, int duration) {
+            super.startScroll(startX, startY, dx, dy, 350 /*1 secs*/);
+        }
+    }
 
     private void init() {
         // The majority of the magic happens here
